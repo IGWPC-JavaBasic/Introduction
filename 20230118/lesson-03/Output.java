2217ac17b1d0f27c648138c48435b5e2965352f0
IGWPC-JavaBasic <htun@osfl.ac.jp>
2023-02-14 15:22:37 +0900

    Java Intro, Concept, CMD

---
+++ 20230118/lesson-03/Output.java
@@ -0,0 +1,12 @@
+public class Output {
+    public static void main(String[] args) {
+        
+        System.out.println("This is String");
+        System.out.println(123);
+        System.out.println(false);
+
+        System.out.println("A is " + 12);
+        System.out.println(12 + 13);
+
+    }
+}
---
+++ 20230118/lesson-04/Comment.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Class Name   : Comment
+ * Description  : Learning Java's Comment
+ * Author       : Htun Myat Maw
+ * Start Date   : 2023/01/18
+ * End Date     : 2023/01/18
+ * 
+ **/
+public class Comment {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("first line");
+        System.out.println("second line");
+        System.out.println("third line");
+ 
+        // System.out.println("fourth line");
+        
+        System.out.println("fifth line");
+
+    }
+
+}
